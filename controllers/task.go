a091949e24534d0e4b27bb822876ee96d064153c
marvzhang <tikazyq@163.com>
2021-06-01 22:22:01 +0800

    added task controller methods

--- controllers/task.go
+++ controllers/task.go
@@ -6,10 +6,12 @@ import (
 	"github.com/crawlab-team/crawlab-core/models/models"
 	"github.com/crawlab-team/crawlab-core/models/service"
 	"github.com/crawlab-team/crawlab-core/spider/admin"
+	clog "github.com/crawlab-team/crawlab-log"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/dig"
 	"net/http"
+	"sync"
 )
 
 var TaskController ListActionController
@@ -25,14 +27,23 @@ var TaskActions = []Action{
 		Path:        "/:id/restart",
 		HandlerFunc: taskCtx.restart,
 	},
+	{
+		Method:      http.MethodGet,
+		Path:        "/:id/logs",
+		HandlerFunc: taskCtx.getLogs,
+	},
 }
 
 type taskContext struct {
 	modelSvc service.ModelService
 	adminSvc interfaces.SpiderAdminService
+	l        clog.Driver
+
+	// internals
+	drivers sync.Map
 }
 
-func (ctx taskContext) run(c *gin.Context) {
+func (ctx *taskContext) run(c *gin.Context) {
 	// task
 	var t models.Task
 	if err := c.ShouldBindJSON(&t); err != nil {
@@ -69,7 +80,7 @@ func (ctx taskContext) run(c *gin.Context) {
 	HandleSuccess(c)
 }
 
-func (ctx taskContext) restart(c *gin.Context) {
+func (ctx *taskContext) restart(c *gin.Context) {
 	// id
 	id, err := primitive.ObjectIDFromHex(c.Param("id"))
 	if err != nil {
@@ -100,11 +111,70 @@ func (ctx taskContext) restart(c *gin.Context) {
 	HandleSuccess(c)
 }
 
+func (ctx *taskContext) getLogs(c *gin.Context) {
+	// id
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		HandleErrorBadRequest(c, err)
+		return
+	}
+
+	// pagination
+	p, err := GetPagination(c)
+	if err != nil {
+		HandleErrorBadRequest(c, err)
+		return
+	}
+
+	// log driver
+	l, err := ctx._getLogDriver(id)
+	if err != nil {
+		HandleErrorInternalServerError(c, err)
+		return
+	}
+
+	// logs
+	logs, err := l.Find("", (p.Page-1)*p.Size, p.Size)
+	if err != nil {
+		HandleErrorInternalServerError(c, err)
+		return
+	}
+	total, err := l.Count("")
+	if err != nil {
+		HandleErrorInternalServerError(c, err)
+		return
+	}
+
+	HandleSuccessWithListData(c, logs, total)
+}
+
+func (ctx *taskContext) _getLogDriver(id primitive.ObjectID) (l clog.Driver, err error) {
+	// attempt to get from cache
+	res, ok := ctx.drivers.Load(id)
+	if ok {
+		l, ok = res.(clog.Driver)
+		if ok {
+			return l, nil
+		}
+	}
+
+	// TODO: other types of log drivers
+	l, err = clog.NewSeaweedFsLogDriver(&clog.SeaweedFsLogDriverOptions{Prefix: id.Hex()})
+	if err != nil {
+		return nil, err
+	}
+	ctx.drivers.Store(id, l)
+
+	return l, nil
+}
+
 var taskCtx = newTaskContext()
 
 func newTaskContext() *taskContext {
 	// context
-	ctx := &taskContext{}
+	ctx := &taskContext{
+		drivers: sync.Map{},
+	}
 
 	// dependency injection
 	c := dig.New()
