ae76a9dcb58029b66718cde3a03712fbd0c99ec3
Marvin Zhang <tikazyq@163.com>
2022-02-19 20:52:13 +0800

    updated basic config

--- config/config.go
+++ config/config.go
@@ -2,8 +2,12 @@ package config
 
 import (
 	"github.com/apex/log"
+	"github.com/crawlab-team/crawlab-core/utils"
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+	"io/ioutil"
+	"os"
+	"path"
 	"strings"
 )
 
@@ -37,17 +41,44 @@ func (c *Config) WatchConfig() {
 }
 
 func (c *Config) Init() error {
+	// config
 	if c.Name != "" {
 		viper.SetConfigFile(c.Name) // if config file is set, load it accordingly
 	} else {
 		viper.AddConfigPath("./conf") // if no config file is set, load by default
 		viper.SetConfigName("config")
 	}
-	viper.SetConfigType("yaml")   // default yaml
-	viper.AutomaticEnv()          // load matched environment variables
+
+	// config type as yaml
+	viper.SetConfigType("yaml") // default yaml
+
+	// auto env
+	viper.AutomaticEnv() // load matched environment variables
+
+	// env prefix
 	viper.SetEnvPrefix("CRAWLAB") // environment variable prefix as CRAWLAB
+
+	// replacer
 	replacer := strings.NewReplacer(".", "_")
 	viper.SetEnvKeyReplacer(replacer)
+
+	// create conf directory if not exists
+	confDirName := "./conf"
+	if !utils.Exists(confDirName) {
+		if err := os.MkdirAll("./conf", os.ModeDir|os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	// add default conf/config.yaml if not exists
+	confFilePath := path.Join(confDirName, "config.yml")
+	if !utils.Exists(confFilePath) {
+		if err := ioutil.WriteFile(confFilePath, []byte(DefaultConfigYaml), os.FileMode(0766)); err != nil {
+			return err
+		}
+	}
+
+	// read config
 	if err := viper.ReadInConfig(); err != nil { // viper parsing config file
 		return err
 	}
